e40101f1bf9fe570b214c771c57c45781e97e910
ViktoriyaKo <kozlova_viktoriy@mail.ru>
2022-11-02 16:31:15 +0300

    feat: add 01-read-file and 02-write-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+//считывание и вывод потока
+const fs = require('fs');
+const path = require('path');
+const pathFile = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(pathFile, 'utf-8');
+
+let data = '';
+stream.on('error', error => console.log('Error', error.message)); // обработка ошибки
+stream.on('data', chunk => data += chunk); // data - событие data, которое генерируется, когда стрим прочитал порцию данных и готов отдать ее потребителю
+stream.on('end', () => console.log(data)); //end-событие срабатывает, когда все данные уже переданы.
+
+
---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const readline = require('readline');
+const { stdin: input, stdout } = require('process');
+const path = require('path');
+const pathFile = path.join(__dirname, 'text.txt');
+const output = fs.createWriteStream(pathFile);
+const rl = readline.createInterface({ input, output });
+
+
+stdout.write('Приветствую! Введите текст: \n'); //обычный вывод console
+
+rl.on('line', (input) => {
+    if(input === 'exit') {
+        console.log('До новых встреч!');
+        process.exit()
+    }
+    output.write(input + '\n')
+  });
+
+process.on('SIGINT', () => {
+    console.log('До новых встреч!');
+    process.exit()
+  });
+
