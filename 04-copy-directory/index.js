154bc4ed0c67c2c29c66b69a9368e0dd4dbde289
ViktoriyaKo <kozlova_viktoriy@mail.ru>
2022-11-09 00:15:26 +0300

    feat: add removing of files in 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,54 @@
 let fs = require("fs");
 const path = require("path");
-const pathFile = path.join(__dirname, "files");
 
-//создание папки
+// создание папки
 fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, (err) => {
   if (err) throw err;
   console.log('Папка "files" успешно скопирована');
 });
 
-//чтение папки
-fs.readdir(pathFile, { withFileTypes: true }, (err, files) => {
-  if (err) throw err;
+let readFrom = path.join(__dirname, "files");
+let copyTo = path.join(__dirname, "files-copy");
 
-  files.forEach((file) => {
-    let filePathOld = path.join(__dirname, "files", file.name);
-    let filePathNew = path.join(__dirname, "files-copy", file.name);
-    //копирование
-    fs.copyFile(filePathOld, filePathNew, (err) => {
-      if (err) throw err;
+function copyFile(pathFile, pathCopy) {
+  fs.readdir(pathFile, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+
+    files.forEach((file) => {
+      if (!file.isFile()) {
+        copyFile(
+          path.join(pathFile, file.name),
+          path.join(pathCopy, file.name)
+        );
+      } else {
+        (async () => {
+          //create => copy
+          try {
+            await fs.promises.mkdir(pathCopy, { recursive: true });
+            let filePathOld = path.join(pathFile, file.name);
+            let filePathNew = path.join(pathCopy, file.name);
+            await fs.promises.copyFile(filePathOld, filePathNew);
+          } catch (error) {
+            throw error;
+          }
+        })();
+      }
+    });
+
+    // удаление повторений
+    fs.promises.readdir(copyTo).then((copyFile) => {
+      let arrayFiles = [];
+      files.forEach((file) => {
+        arrayFiles.push(file.name);
+      });
+
+      let extraFile = copyFile.filter((item) => !arrayFiles.includes(item));
+
+      extraFile.forEach((extFile) => {
+        fs.promises.rm(path.join(copyTo, extFile));
+      });
     });
   });
-});
+}
+
+copyFile(readFrom, copyTo);
